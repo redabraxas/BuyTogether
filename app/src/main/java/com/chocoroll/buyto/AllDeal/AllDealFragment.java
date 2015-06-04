bf97841fcbb18c8e3325f8fbc1c7fd3637a0da1e
hjnam0404 <hjnam0404@naver.com>
2015-06-05 01:30:55 +0900

    HyeJI

--- app/src/main/java/com/chocoroll/buyto/AllDeal/AllDealFragment.java
+++ app/src/main/java/com/chocoroll/buyto/AllDeal/AllDealFragment.java
@@ -10,25 +10,23 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.chocoroll.buyto.DetailDeal.DetailDealActivity;
+import com.chocoroll.buyto.Extra.Retrofit;
 import com.chocoroll.buyto.MainActivity;
 import com.chocoroll.buyto.Model.BookMark;
 import com.chocoroll.buyto.Model.Deal;
 import com.chocoroll.buyto.Model.DealAdapter;
 import com.chocoroll.buyto.R;
-import com.chocoroll.buyto.Extra.Retrofit;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
@@ -183,13 +181,16 @@ public class AllDealFragment extends Fragment {
         (v.findViewById(R.id.bookMark)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(((MainActivity)MainActivity.mContext).getUserId().equals("")){
+                    Toast.makeText(getActivity(),"로그인한 유저만 사용할 수 있습니다.",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    String bCategory = spinnerB.getSelectedItem().toString();
 
-
-                String bCategory = spinnerB.getSelectedItem().toString();
-
-                if (!bCategory.equals("전체보기")) {
-                    String sCategory = spinnerS.getSelectedItem().toString();
-                    addBookMark(bCategory, sCategory);
+                    if (!bCategory.equals("전체보기")) {
+                        String sCategory = spinnerS.getSelectedItem().toString();
+                        addBookMark(bCategory, sCategory);
+                    }
                 }
 
             }
--- app/src/main/java/com/chocoroll/buyto/DetailDeal/DetailDialog.java
+++ app/src/main/java/com/chocoroll/buyto/DetailDeal/DetailDialog.java
@@ -2,23 +2,14 @@ package com.chocoroll.buyto.DetailDeal;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.chocoroll.buyto.Extra.DownloadImageTask;
 import com.chocoroll.buyto.R;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 /**
  * Created by HyeJi on 2015. 6. 4..
@@ -43,7 +34,7 @@ public class DetailDialog extends Dialog{
                 .execute(url);
 
 
-        Button btn = (Button)findViewById(R.id.btnFinish);
+        TextView btn = (TextView)findViewById(R.id.btnFinish);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
