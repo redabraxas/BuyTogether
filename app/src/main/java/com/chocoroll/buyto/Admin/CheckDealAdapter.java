02db66fb263c66ef69a8f5d29682ba09776be1cb
redabraxas <udhee94@nate.com>
2015-06-04 18:51:38 +0900

    DANHEE

--- app/src/main/java/com/chocoroll/buyto/Admin/CheckDealAdapter.java
+++ app/src/main/java/com/chocoroll/buyto/Admin/CheckDealAdapter.java
@@ -38,34 +38,39 @@ public CheckDealAdapter(Context context, int textViewResourceId, ArrayList<Deal>
         this.context = context;
         }
 
-public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
         View v = convertView;
         if (v == null) {
-        LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        v = vi.inflate(R.layout.model_check_deal, null);
+            LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = vi.inflate(R.layout.model_check_deal, null);
         }
-final Deal p = items.get(position);
+
+        final Deal p = items.get(position);
         if (p != null) {
+
             new DownloadImageTask((ImageView) v.findViewById(R.id.thumbnailDeal))
                     .execute(p.getThumbnail());
 
-            String str = "["+p.getbCategory()+"/"+p.getsCategory()+"]";
-            ((TextView)v.findViewById(R.id.price)).setText(p.getPrice()+" 원");
-            ((TextView)  v.findViewById(R.id.txt_category)).setText(str);
+            String str = "[" + p.getbCategory() + "/" + p.getsCategory() + "]";
+            ((TextView) v.findViewById(R.id.price)).setText(p.getPrice() + " 원");
+            ((TextView) v.findViewById(R.id.txt_category)).setText(str);
             ((TextView) v.findViewById(R.id.txt_name)).setText(p.getName());
             ((TextView) v.findViewById(R.id.txt_dday)).setText(p.getDday());
-            ((TextView)  v.findViewById(R.id.txt_people)).setText(String.valueOf(p.getBook()+"/"+p.getMaxBook()));
+            ((TextView) v.findViewById(R.id.txt_people)).setText(String.valueOf(p.getBook() + "/" + p.getMaxBook()));
 
             if(p.getLevel().equals("seller")){
-                ((LinearLayout) v.findViewById(R.id.level_bar)).setBackgroundColor(context.getResources().getColor(R.color.seller));
+                ( v.findViewById(R.id.level_bar)).setBackgroundColor(context.getResources().getColor(R.color.seller));
+                ((TextView)v.findViewById(R.id.level_bar)).setText("SELLER");
             }else{
-                ((LinearLayout) v.findViewById(R.id.level_bar)).setBackgroundColor(context.getResources().getColor(R.color.user));
+                ( v.findViewById(R.id.level_bar)).setBackgroundColor(context.getResources().getColor(R.color.user));
+                ((TextView)v.findViewById(R.id.level_bar)).setText("USER");
             }
 
             ((TextView) v.findViewById(R.id.btnOK)).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
+                    sendDealResult(p.getNum(), "ok");
 
 
                 }
@@ -76,107 +81,111 @@ final Deal p = items.get(position);
                 @Override
                 public void onClick(View view) {
 
+                    sendDealResult(p.getNum(), "ok");
 
 
                 }
             });
 
 
-
         }
         return v;
-        }
-
-        void sendsSellerResult(String id, String result){
+    }
 
 
-final String str;
+
+
+    void sendDealResult(String dealNum, String result){
+
+
+        final String str;
         if(result.equals("ok")){
-        str ="승인";
+            str ="승인";
         }else{
-        str="거절";
+            str="거절";
         }
 
 
-final ProgressDialog dialog = new ProgressDialog(context);
-        dialog.setMessage("판매자를 "+str+"하는 중입니다...");
+        final ProgressDialog dialog = new ProgressDialog(context);
+        dialog.setMessage("딜을 "+str+"하는 중입니다...");
         dialog.setIndeterminate(true);
         dialog.setCancelable(false);
         dialog.show();
 
-final JsonObject info = new JsonObject();
-        info.addProperty("id", id);
+        final JsonObject info = new JsonObject();
+        info.addProperty("dealNum", dealNum);
         info.addProperty("result", result);
 
 
         new Thread(new Runnable() {
-public void run() {
-        try {
+            public void run() {
+                try {
 
-        RestAdapter restAdapter = new RestAdapter.Builder()
-        .setEndpoint(Retrofit.ROOT)  //call your base url
-        .build();
-        Retrofit retrofit = restAdapter.create(Retrofit.class); //this is how retrofit create your api
-        retrofit.sendsSellerResult(info, new Callback<String>() {
+                    RestAdapter restAdapter = new RestAdapter.Builder()
+                            .setEndpoint(Retrofit.ROOT)  //call your base url
+                            .build();
+                    Retrofit retrofit = restAdapter.create(Retrofit.class); //this is how retrofit create your api
+                    retrofit.sendDealResult(info, new Callback<String>() {
 
-@Override
-public void success(String result, Response response) {
+                        @Override
+                        public void success(String result, Response response) {
 
-        dialog.dismiss();
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                            dialog.dismiss();
+                            AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
 
-        if(result.equals("success")){
-        builder.setTitle("성공")        // 제목 설정
-        .setMessage("판매자를 "+str+"하셨습니다.")        // 메세지 설정
-        .setCancelable(false)        // 뒤로 버튼 클릭시 취소 가능 설정
-        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-// 확인 버튼 클릭시 설정
-public void onClick(DialogInterface dialog, int whichButton) {
-        }
-        });
+                            if(result.equals("success")){
+                                builder.setTitle("성공")        // 제목 설정
+                                        .setMessage("딜을 "+str+"하셨습니다.")        // 메세지 설정
+                                        .setCancelable(false)        // 뒤로 버튼 클릭시 취소 가능 설정
+                                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                            // 확인 버튼 클릭시 설정
+                                            public void onClick(DialogInterface dialog, int whichButton) {
+                                            }
+                                        });
 
-        }else{
-        builder.setTitle("판매자 "+str+"을 실패하였습니다.")        // 제목 설정
-        .setMessage("네트워크를 확인해주세요")        // 메세지 설정
-        .setCancelable(false)        // 뒤로 버튼 클릭시 취소 가능 설정
-        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-// 확인 버튼 클릭시 설정
-public void onClick(DialogInterface dialog, int whichButton) {
-        }
-        });
+                            }else{
+                                builder.setTitle("딜의 "+str+"을 실패하였습니다.")        // 제목 설정
+                                        .setMessage("네트워크를 확인해주세요")        // 메세지 설정
+                                        .setCancelable(false)        // 뒤로 버튼 클릭시 취소 가능 설정
+                                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                            // 확인 버튼 클릭시 설정
+                                            public void onClick(DialogInterface dialog, int whichButton) {
+                                            }
+                                        });
 
-        }
-        AlertDialog dialog = builder.create();    // 알림창 객체 생성
-        dialog.show();    // 알림창 띄우기
+                            }
+                            AlertDialog dialog = builder.create();    // 알림창 객체 생성
+                            dialog.show();    // 알림창 띄우기
 
-        }
+                        }
 
-@Override
-public void failure(RetrofitError retrofitError) {
+                        @Override
+                        public void failure(RetrofitError retrofitError) {
 
-        dialog.dismiss();
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setTitle("네트워크가 불안정합니다.")        // 제목 설정
-        .setMessage("네트워크를 확인해주세요")        // 메세지 설정
-        .setCancelable(false)        // 뒤로 버튼 클릭시 취소 가능 설정
-        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-// 확인 버튼 클릭시 설정
-public void onClick(DialogInterface dialog, int whichButton) {
-        }
-        });
+                            dialog.dismiss();
+                            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                            builder.setTitle("네트워크가 불안정합니다.")        // 제목 설정
+                                    .setMessage("네트워크를 확인해주세요")        // 메세지 설정
+                                    .setCancelable(false)        // 뒤로 버튼 클릭시 취소 가능 설정
+                                    .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                        // 확인 버튼 클릭시 설정
+                                        public void onClick(DialogInterface dialog, int whichButton) {
+                                        }
+                                    });
 
-        AlertDialog dialog = builder.create();    // 알림창 객체 생성
-        dialog.show();    // 알림창 띄우기
+                            AlertDialog dialog = builder.create();    // 알림창 객체 생성
+                            dialog.show();    // 알림창 띄우기
 
-        }
-        });
-        } catch (Throwable ex) {
+                        }
+                    });
+                } catch (Throwable ex) {
 
-        }
-        }
+                }
+            }
         }).start();
 
-        }
+    }
 
-        }
+}
+
